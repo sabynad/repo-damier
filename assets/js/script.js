af8bb9788ec46afe8a68c0cc141310c4f956ce2a
sabynad <chafroud.sabri@hotmail.fr>
2023-10-09 16:47:53 +0200

    damier-v2

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// pour s'assurer que le scripte fonctione dans la console permet de verifier avant de coder
+console.log("script chargé !");
+
+// creation d'un element table qui s'inclut dans le damier
+let monDamier = document.createElement("table");
+// j'intègre monDmier dans le body
+document.body.appendChild(monDamier);
+
+// génération des lignes du damier
+
+
+
